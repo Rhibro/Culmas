36ecd8e26cc87410852d3a7e5fb33b59469713ba
Rhibro <111956763+Rhibro@users.noreply.github.com>
2023-06-27 13:08:38 +0200

    Working on making pairs

--- script.js
+++ script.js
@@ -1,9 +1,30 @@
+function getnewarray () {
+
 const randomArr = Array.from({length: 9},
      
     () => Math.floor(Math.random() * 25) + 1);
       
     document.getElementById("array").innerHTML = JSON.stringify(randomArr);
 
+};
+
+console.log()
+
+//function makecombos () {
+
+    const SumKth = (randomArr, k) => {
+        const pair = [];
+        for(let i = 0; i < randomArr.length; i++){
+          for(let j = i + 1; j < randomArr.length; j++){ //o(n)
+             if([i] + randomArr[j] == k){
+              pair.push(randomArr[i]);
+              pair.push(randomArr[j]);
+             }
+          }
+        }
+        return pair;
+      }
+console.log(SumKth)
 
 
 
