7bbfb7c175f24a2e0de448b71a81e50c08374354
Rhibro <111956763+Rhibro@users.noreply.github.com>
2023-06-29 09:55:55 +0200

    working on getting pairs

--- script.js
+++ script.js
@@ -1,46 +1,13 @@
 function getnewarray () {
 
-const randomArr = Array.from({length: 9},
+  const randomArr = Array.from({length: 9},
      
-    () => Math.floor(Math.random() * 25) + 1);
+    () => Math.floor(Math.random() * 20) + 1);
       
     document.getElementById("array").innerHTML = JSON.stringify(randomArr);
 
-};
+  };
 
-console.log()
-
-//function makecombos () {
-
-    const SumKth = (randomArr, k) => {
-        const pair = [];
-        for(let i = 0; i < randomArr.length; i++){
-          for(let j = i + 1; j < randomArr.length; j++){ //o(n)
-             if([i] + randomArr[j] == k){
-              pair.push(randomArr[i]);
-              pair.push(randomArr[j]);
-             }
-          }
-        }
-        return pair;
-      }
-console.log(SumKth)
+   
 
 
-
-/*const randomArray = (min, max, n = 1) =>
-Array.from(
-    { length: n },
-    () => Math.floor(Math.random() * (max - min + 1)) + min
-);
-
-console.log(randomArray())*/
-
-
-/*const arrayLength = 10 
-const randomArray = []
-for(let i = 0; i<arrayLength; i++) {randomArray.push(Math.random())}
-
-console.log(randomArray)*/
-
-//document.getElementById('array').innerHTML = Math.floor(Math.random() * 25) + 1
