0ec2d842d3876170e8bf021eaf5da984727d59f0
Rhibro <111956763+Rhibro@users.noreply.github.com>
2023-06-29 11:50:00 +0200

    Working on it still

--- script.js
+++ script.js
@@ -8,6 +8,44 @@ function getnewarray () {
 
   };
 
-   
-
-
+  /*function findCombinations(array) {
+    const result = [];
+  
+    // Helper function to check if a combination adds up to 21
+    function isCombinationSum21(combination) {
+      return combination.reduce((sum, num) => sum + num, 0) === 21;
+    }
+  
+    // Generate all combinations of two numbers
+    for (let i = 0; i < array.length; i++) {
+      for (let j = i + 1; j < array.length; j++) {
+        const combination = [array[i], array[j]];
+        if (isCombinationSum21(combination)) {
+          result.push([{ index: i, value: array[i] }, { index: j, value: array[j] }]);
+        }
+      }
+    }
+  
+    return result;
+  }
+  
+  // Generate a randomized array of 9 numbers not exceeding 20
+  function generateRandomArray() {
+    const array = [];
+    while (array.length < 9) {
+      const number = Math.floor(Math.random() * 21) + 1;
+      if (!array.includes(number)) {
+        array.push(number);
+      }
+    }
+    return array;
+  }
+  
+  // Example usage
+  const array = generateRandomArray();
+  const combinations = findCombinations(array);
+  
+  console.log('Array:', array);
+  console.log('Combinations:', combinations); */
+  
+  
