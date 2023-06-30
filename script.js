b3724548867d8d019adf5daa23de2bb8141fcd98
Rhibro <111956763+Rhibro@users.noreply.github.com>
2023-06-30 15:00:27 +0200

    works if I keep reloading the page

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function getnewarray () {
+/*function getnewarray () {
 
   const randomArr = Array.from({length: 9},
      
@@ -8,15 +8,15 @@ function getnewarray () {
 
   };
 
-  /*function findCombinations(array) {
+  const array = getnewarray();
+
+ function findCombinations(array) {
     const result = [];
   
-    // Helper function to check if a combination adds up to 21
     function isCombinationSum21(combination) {
       return combination.reduce((sum, num) => sum + num, 0) === 21;
     }
   
-    // Generate all combinations of two numbers
     for (let i = 0; i < array.length; i++) {
       for (let j = i + 1; j < array.length; j++) {
         const combination = [array[i], array[j]];
@@ -25,27 +25,49 @@ function getnewarray () {
         }
       }
     }
+
+    document.getElementById("comboo").innerHTML = JSON.stringify(result);
+
+     return result;
+  };*/
+
   
-    return result;
-  }
-  
-  // Generate a randomized array of 9 numbers not exceeding 20
-  function generateRandomArray() {
-    const array = [];
-    while (array.length < 9) {
-      const number = Math.floor(Math.random() * 21) + 1;
-      if (!array.includes(number)) {
-        array.push(number);
-      }
+   
+    
+    let array;
+
+    function getNewArray() {
+      const randomArr = Array.from({ length: 9 }, () => Math.floor(Math.random() * 20) + 1);
+      document.getElementById("array").innerHTML = JSON.stringify(randomArr);
+      return randomArr;
     }
-    return array;
-  }
-  
-  // Example usage
-  const array = generateRandomArray();
-  const combinations = findCombinations(array);
-  
-  console.log('Array:', array);
-  console.log('Combinations:', combinations); */
-  
-  
+    
+    function findCombinations(array) {
+      const result = [];
+    
+      function isCombinationSum21(combination) {
+        return combination.reduce((sum, num) => sum + num, 0) === 21;
+      }
+    
+      for (let i = 0; i < array.length; i++) {  //this is where the problem is keep gettting the error "Uncaught TypeError: Cannot read properties of undefined (reading 'length')"
+        for (let j = i + 1; j < array.length; j++) {
+          const combination = [array[i], array[j]];
+          if (isCombinationSum21(combination)) {
+            result.push([{ index: i, value: array[i] }, { index: j, value: array[j] }]);
+          }
+        }
+      }
+    
+      document.getElementById("comboo").innerHTML = JSON.stringify(result);
+      return result;
+    }
+    
+    document.addEventListener("DOMContentLoaded", function() {
+      array = getNewArray();
+    
+      const button = document.getElementById("myButton");
+      button.addEventListener("click", function() {
+        findCombinations(array);
+      });
+    });
+    
