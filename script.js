92f5d5b706f2d6ef4fd89bf5747435038948507e
Rhibro <111956763+Rhibro@users.noreply.github.com>
2023-07-01 14:57:45 +0200

    only works when page is reloaded

--- script.js
+++ script.js
@@ -31,46 +31,43 @@
      return result;
   }; */
 
-  
-  
-    
-    let array;
+  let array;
 
-    function getNewArray() {
-      const randomArr = Array.from({ length: 9 }, () => Math.floor(Math.random() * 20) + 1);
-      document.getElementById("array").innerHTML = JSON.stringify(randomArr);
-      return randomArr;
-    }
-    
-    
-    function findCombinations(array) {
-      const result = [];
-    
-      function isCombinationSum21(combination) {
-        return combination.reduce((sum, num) => sum + num, 0) === 21;
-      }
+function getNewArray() {
+  const randomArr = Array.from({ length: 9 }, () => Math.floor(Math.random() * 20) + 1);
+  document.getElementById("array").innerHTML = JSON.stringify(randomArr);
+  return randomArr;
+}
 
-     
-    
-      for (let i = 0; i < array.length; i++) {  //this is where the problem is keep gettting the error "Uncaught TypeError: Cannot read properties of undefined (reading 'length')"
-        for (let j = i + 1; j < array.length; j++) {
-          const combination = [array[i], array[j]];
-          if (isCombinationSum21(combination)) {
-            result.push([{ index: i, value: array[i] }, { index: j, value: array[j] }]);
-          }
-        }
+function findCombinations(array) {
+  const result = [];
+
+  function isCombinationSum21(combination) {
+    return combination.reduce((sum, num) => sum + num, 0) === 21;
+  }
+
+  for (let i = 0; i < array.length; i++) {
+    for (let j = i + 1; j < array.length; j++) {
+      const combination = [array[i], array[j]];
+      if (isCombinationSum21(combination)) {
+        result.push([{ index: i, value: array[i] }, { index: j, value: array[j] }]);
       }
-    
-      document.getElementById("comboo").innerHTML = JSON.stringify(result);
-      return result;
     }
-    
-    document.addEventListener("DOMContentLoaded", function() {
-      array = getNewArray();
-    
-      const button = document.getElementById("myButton");
-      button.addEventListener("click", function() {
-        findCombinations(array);
-      });
-    });
-    
+  }
+
+  document.getElementById("comboo").innerHTML = JSON.stringify(result);
+  return result;
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+  array = getNewArray(); // Assign the array in the global scope
+
+  const button = document.getElementById("myButton");
+  button.addEventListener("click", function() {
+    findCombinations(array);
+  });
+});
+
+
+
+ 
