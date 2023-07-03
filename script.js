cf2ad7ed2530ff4b56bf0f221fe4941598023028
Rhibro <111956763+Rhibro@users.noreply.github.com>
2023-07-03 13:22:42 +0200

    it works if you reload the page

--- script.js
+++ script.js
@@ -1,37 +1,7 @@
-/* function getnewarray () {
+//it works but the page needs to be refreshed to get get new results. 
+// I had an error on line 19 that I wasn't sure how to resolve. 
 
-  const randomArr = Array.from({length: 9},
-     
-    () => Math.floor(Math.random() * 20) + 1);
-      
-    document.getElementById("array").innerHTML = JSON.stringify(randomArr);
-
-  };
-
-  const array = getnewarray();
-
- function findCombinations(array) {
-    const result = [];
-  
-    function isCombinationSum21(combination) {
-      return combination.reduce((sum, num) => sum + num, 0) === 21;
-    }
-  
-    for (let i = 0; i < array.length; i++) {
-      for (let j = i + 1; j < array.length; j++) {
-        const combination = [array[i], array[j]];
-        if (isCombinationSum21(combination)) {
-          result.push([{ index: i, value: array[i] }, { index: j, value: array[j] }]);
-        }
-      }
-    }
-
-    document.getElementById("comboo").innerHTML = JSON.stringify(result);
-
-     return result;
-  }; */
-
-  let array;
+let array;
 
 function getNewArray() {
   const randomArr = Array.from({ length: 9 }, () => Math.floor(Math.random() * 20) + 1);
@@ -46,11 +16,11 @@ function findCombinations(array) {
     return combination.reduce((sum, num) => sum + num, 0) === 21;
   }
 
-  for (let i = 0; i < array.length; i++) {
+  for (let i = 0; i < array.length; i++) {  //had trouble with an error here where it says the property is undefined
     for (let j = i + 1; j < array.length; j++) {
       const combination = [array[i], array[j]];
       if (isCombinationSum21(combination)) {
-        result.push([{ index: i, value: array[i] }, { index: j, value: array[j] }]);
+        result.push( i , '&' , j );
       }
     }
   }
@@ -60,7 +30,7 @@ function findCombinations(array) {
 }
 
 document.addEventListener("DOMContentLoaded", function() {
-  array = getNewArray(); // Assign the array in the global scope
+  array = getNewArray();
 
   const button = document.getElementById("myButton");
   button.addEventListener("click", function() {
